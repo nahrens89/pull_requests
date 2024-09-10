54450eba93060b1bae00702e7f88f4eddefd4e03
nahrens89 <111275890+nahrens89@users.noreply.github.com>
2024-09-10 14:25:42 +0200

    Initial commit

---
+++ src/feature.py
@@ -0,0 +1,46 @@
+"""
+Feature Module Description
+"""
+
+
+def feature_1() -> None:
+    """
+    Function Description
+    """
+    print("Feature One")
+    print("Now it will work!")
+
+
+def feature_two() -> None:
+    """
+    Function Description
+    """
+    print("Feature Two")
+
+
+def feature_three() -> None:
+    """
+    Function Description
+    """
+    print("Feature Two")
+
+
+def feature_four_0() -> None:
+    """
+    Description added
+    """
+    print("Feature Four point zero")
+
+
+def feature_four_1():
+    """
+    Description changed
+    """
+    print("Feature Four point one")
+
+
+def feature_nico():
+    """
+    This is a feature written bei Nico
+    """
+    print("Everything is awesome!")
